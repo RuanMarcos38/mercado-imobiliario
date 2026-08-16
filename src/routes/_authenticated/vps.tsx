afaa7bc288f5712e236cd378893971654b318bb6
RuanMarcos38 <ruanessencia8@gmail.com>
2026-08-16 08:01:40 -0300

    Remove obsolete VPS route from property search platform

--- src/routes/_authenticated/vps.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_authenticated/vps")({
-  beforeLoad: () => {
-    throw redirect({ to: "/dashboard" });
-  },
-  component: () => null,
-});
