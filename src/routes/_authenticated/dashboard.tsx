ef2ceabd4dde8e903613abcba77d5b0cf4746b20
RuanMarcos38 <ruanessencia8@gmail.com>
2026-08-16 21:04:21 -0300

    feat: open listing phones in atendimento

--- src/routes/_authenticated/dashboard.tsx
+++ src/routes/_authenticated/dashboard.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { PropertyWorkspace } from "@/components/property/PropertyWorkspace";
+import { PropertyWorkspaceAtendimento } from "@/components/property/PropertyWorkspaceAtendimento";
 
 export const Route = createFileRoute("/_authenticated/dashboard")({
   component: DashboardPage,
@@ -16,5 +16,5 @@ export const Route = createFileRoute("/_authenticated/dashboard")({
 });
 
 function DashboardPage() {
-  return <PropertyWorkspace initialMarket="all" />;
+  return <PropertyWorkspaceAtendimento initialMarket="all" />;
 }
